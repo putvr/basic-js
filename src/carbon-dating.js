0f0b5d8bb8cb8301fbaa86e0aa4d9bd170a4115d
Roman Putilov <Putilov.Roman@gmail.com>
2022-04-30 12:12:29 +0500

    Carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,26 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) != 'string') {
+    return false;
+  }
+
+  const n = Number(sampleActivity);
+
+  if(Number.isNaN(n)) {
+    return false;
+  }
+
+  if(n < 1 || n > MODERN_ACTIVITY) {
+    return false;
+  }
+
+  return Math.ceil(
+    Math.log(MODERN_ACTIVITY / n) 
+    / 
+    (0.693 / HALF_LIFE_PERIOD)
+  );
 }
 
 module.exports = {
