8b9ded76d7bdad2526aad309700b7db066370280
Roman Putilov <Putilov.Roman@gmail.com>
2022-04-30 12:45:00 +0500

    Vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  type; 
+  __decrypt;
+
+  constructor(type=true) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, key) {
+    this.__decrypt = false;
+    return this.__vegener(str, key);
+  }
+  
+  decrypt(str, key) {
+    this.__decrypt = true;
+    return this.__vegener(str, key);  
+  }
+
+  __vegener(str, key) {
+    if (str == undefined || key == undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let text = [];
+    let ki=0;
+    const offset = this.__decrypt ? 26 : 0;
+    const prefix = this.__decrypt ? -1 : 1;
+
+    str = str.toUpperCase();
+    key = key.padEnd(str.length, key).toUpperCase();
+
+    for(let i = 0; i < str.length; i++) {
+      if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {  
+        const diff = str.charCodeAt(i) + offset + (prefix * key.charCodeAt(ki));
+        text += String.fromCharCode(diff % 26 + 65);
+        ki++;
+        continue;
+      } 
+        
+      text +=str[i];
+    }
+    return this.type ? text : text.split('').reverse().join('');    
   }
 }
 
